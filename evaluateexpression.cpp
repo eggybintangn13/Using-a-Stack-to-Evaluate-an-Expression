2a7f5cef32f0f99221f0d9de3dcc1275f3807d88
Eggy Bintang Negara 1013 <48612244+eggybintangn13@users.noreply.github.com>
2019-04-23 21:39:11 +0700

    Update evaluateexpression.cpp

--- evaluateexpression.cpp
+++ evaluateexpression.cpp
@@ -1,38 +1,37 @@
 n=0;
-	while(postfix[n]!='\0'){
-		
-    if(isdigit(postfix[n])){
-			S.pushtostack(postfix[n]);
-		}
-		
-    else{
-			A=S.popfromstack()-'0';
-			B=S.popfromstack()-'0';
-			
-      if(postfix[n]=='*'){
-				C=B*A;
-				S.pushtostack(C+'0');
-			}
-			
-      else if(postfix[n]=='/'){
-				C=B/A;
-				S.pushtostack(C+'0');
-			}
-			
-      else if(postfix[n]=='+'){
-				C=B+A;
-				S.pushtostack(C+'0');	
-			}
-			
-      else if(postfix[n]=='-'){
-				C=B-A;
-				S.pushtostack(C+'0');
-			}
-			
-      else{
-				cout<<"Stack tidak ada"<<endl;
-			}
-		}
-		n++;
+while(postfix[n]!='\0'){
+	if(isdigit(postfix[n])){
+		S.pushtostack(postfix[n]);
 	}
-	cout<<S.popfromstack()-'0';
+	
+	else{
+		A=S.popfromstack()-'0';
+		B=S.popfromstack()-'0';
+		
+		if(postfix[n]=='*'){
+			C=B*A;
+			S.pushtostack(C+'0');
+		}
+			
+      		else if(postfix[n]=='/'){
+			C=B/A;
+			S.pushtostack(C+'0');
+		}
+			
+      		else if(postfix[n]=='+'){
+			C=B+A;
+			S.pushtostack(C+'0');	
+		}
+			
+      		else if(postfix[n]=='-'){
+			C=B-A;
+			S.pushtostack(C+'0');
+		}
+			
+      		else{
+			cout<<"Stack tidak ada"<<endl;
+		}
+	}
+	n++;
+}
+cout<<S.popfromstack()-'0';

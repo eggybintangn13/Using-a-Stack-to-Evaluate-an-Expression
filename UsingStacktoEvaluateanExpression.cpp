a789be869d6b4b64925320ac3a40c13e858b9516
Eggy Bintang Negara 1013 <48612244+eggybintangn13@users.noreply.github.com>
2019-04-24 18:54:30 +0700

    Update UsingStacktoEvaluateanExpression.cpp

--- UsingStacktoEvaluateanExpression.cpp
+++ UsingStacktoEvaluateanExpression.cpp
@@ -8,66 +8,60 @@ struct node{
 
 class stack{
   private:
-		node *head=NULL;
+	node *head=NULL;
   public:
-    bool isEmpty(){
-      if(head==NULL)
-				return true;
-			return false;
+	bool isEmpty(){
+		if(head==NULL)
+			return true;
+		return false;
 		}
-    
-    void pushtostack(char a){
-			node *baru=new node;
-			baru->element=a;
-			baru->next=NULL;
-			
-      if(isEmpty()){
-				head=baru;
-			}
-      else{
-				baru->next=head;
-				head=baru;
-			}
+	void pushtostack(char a){
+		node *baru=new node;
+		baru->element=a;
+		baru->next=NULL;
+		if(isEmpty()){
+			head=baru;
 		}
-    
-    char popfromstack(){
-      if(isEmpty()){
-				cout<<"Stack kosong!"<<endl;
-				return 0;
-			}
-      else{
-				char ch=head->element;
-				node *tmp=head->next;
-				delete head;
-				head=tmp;
-				return ch;
-			}
+		else{
+			baru->next=head;
+			head=baru;
 		}
-		
-    char top(){
-			return head->element;
+	}
+    	char popfromstack(){
+		if(isEmpty()){
+			cout<<"Stack kosong!"<<endl;
+			return 0;
 		}
-		
-    void cetak(){
-      if(isEmpty()){
-				cout<<"Stack Kosong!";
-			}
-      else{
-				node *bantu=head;
-				while(bantu!=NULL){
-					cout<<bantu->element<<endl;
-					bantu=bantu->next;
-				}
+      		else{
+			char ch=head->element;
+			node *tmp=head->next;
+			delete head;
+			head=tmp;
+			return ch;
+		}
+	}
+    	char top(){
+		return head->element;
+	}	
+    	void cetak(){
+      		if(isEmpty()){
+			cout<<"Stack Kosong!";
+		}
+      		else{
+			node *bantu=head;
+			while(bantu!=NULL){
+			cout<<bantu->element<<endl;
+			bantu=bantu->next;
 			}
 		}
-    
+	}    
 };
 
 int cek(char cekoprtr){	
-  if(cekoprtr=='*'||cekoprtr=='/')
-	return 2;
+	if(cekoprtr=='*'||cekoprtr=='/')
+		return 2;
 	else
-	return 1;
+		return 1;
 }
 
 int main(){
@@ -77,17 +71,14 @@ int main(){
 	char infix[20];
   
 	cin>>infix;
-	
 	while(infix[i]!='\0'){
-    if(isdigit(infix[i])){
+		if(isdigit(infix[i])){
 			postfix[n]=infix[i];
 			n++;
 		}
-    
 		else if(infix[i]=='('){
 			S.pushtostack(infix[i]);
 		}
-    
 		else if(infix[i]==')'){
 			while(!S.isEmpty() && S.top()!='('){
 				postfix[n]=S.popfromstack();
@@ -95,12 +86,10 @@ int main(){
 			}
 			S.popfromstack();
 		}
-    
 		else{
 			if(S.isEmpty() || S.top()=='('){
 				S.pushtostack(infix[i]);
 			}
-      
 			else{
 				while(!S.isEmpty() && S.top()!='(' && cek(infix[i])<=cek(S.top())){
 					postfix[n]=S.popfromstack();
@@ -108,63 +97,43 @@ int main(){
 				}
 				S.pushtostack(infix[i]);
 			}
-      
 		}
 		i++;
-    
 	}
-  
 	while(!S.isEmpty()){
 		postfix[n]=S.popfromstack();
 		n++;
 	}
-  
-	/*postfix[n]='.';
-	n=0;
-	while(postfix[n]!='.'){
-		cout<<postfix[n];
-		n++;
-	}
-	cout<<endl;*/
-  
-  n=0;
-  while(postfix[n]!='\0'){  
-    if(isdigit(postfix[n])){
-		  S.pushtostack(postfix[n]);
-	  }
-	  else{
-		  A=S.popfromstack()-'0';
-		  B=S.popfromstack()-'0';
-		
-		  if(postfix[n]=='*'){
-			  C=B*A;
-			  S.pushtostack(C+'0');
-		  }
-			
-      else if(postfix[n]=='/'){
-			  C=B/A;
-			  S.pushtostack(C+'0');
-		  }
-			
-      else if(postfix[n]=='+'){
-			  C=B+A;
-			  S.pushtostack(C+'0');	
-		  }
-			
-      else if(postfix[n]=='-'){
-			  C=B-A;
-			  S.pushtostack(C+'0');
-		  }
-			
-      else{
-			  cout<<"Stack tidak ada"<<endl;
-		  }
-    
-    }
-	  n++;
-  }
-  
-  cout<<S.popfromstack()-'0';
-	
-  return 0;
+  	n=0;
+  	while(postfix[n]!='\0'){  
+    		if(isdigit(postfix[n])){
+			S.pushtostack(postfix[n]);
+		}
+	  	else{
+		 	A=S.popfromstack()-'0';
+		 	B=S.popfromstack()-'0';
+			if(postfix[n]=='*'){
+				C=B*A;
+				S.pushtostack(C+'0');
+		  	}
+      			else if(postfix[n]=='/'){
+			 	C=B/A;
+		 		S.pushtostack(C+'0');
+		  	}
+			else if(postfix[n]=='+'){
+			 	C=B+A;
+				S.pushtostack(C+'0');	
+		  	}
+			else if(postfix[n]=='-'){
+				C=B-A;
+				S.pushtostack(C+'0');
+		  	}
+			else{
+				cout<<"Stack tidak ada"<<endl;
+		  	}
+		}
+	  	n++;
+  	}
+	cout<<S.popfromstack()-'0';
+	return 0;
 }
